04f2d44c48b7af0774657bbe447440e638a3da1d
Douglas C Machado <douglasclaudino10.dc@gmail.com>
2022-05-30 14:38:19 -0300

    db connection

--- server.ts
+++ server.ts
@@ -1,5 +1,7 @@
 import { app } from "./src/app"
 
-app.listen(4000, () => {
-  console.log("Server is running on port 4000 🚀")
+app.on('ready', () => {
+  app.listen(4000, () => {
+    console.log("Server is running on port 4000 🚀")
+  })
 })
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
+import dotenv from "dotenv"
+dotenv.config()
+
 import express from "express";
 import cors from "cors"
 import { routes } from "./routes";
+import mongoose from "mongoose";
 
 const app = express()
+
+mongoose.connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    console.log('DB working')
+    app.emit('ready')
+  }).catch(err => {
+    console.log(err.message)
+  })
+
 
 app.use(cors())
 app.use(express.json())
