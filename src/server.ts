d284daf99f5868e95a001aea6423a4bd0d0ed8eb
Douglas C Machado <douglasclaudino10.dc@gmail.com>
2022-05-31 17:30:37 -0300

    deploy port env

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { app } from "./app"
 
 app.on('ready', () => {
-  app.listen(4000, () => {
+  app.listen(process.env.PORT || 4000, () => {
     console.log("Server is running on port 4000 🚀")
   })
 })
